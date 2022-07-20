25bb756582b1a26f3410fe8e9fbb2ce0029ef2b5
Matheus <90228238+ofmxtheuuz@users.noreply.github.com>
2022-07-20 12:25:45 -0300

    Add files via upload

---
+++ rooms/Entities/Reservation.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace rooms.Entities
+{
+    class Reservation
+    {
+        public int numberRoom { get; set; }
+        public DateTime checkin { get; set; }
+        public DateTime checkout { get; set; }
+
+        public Reservation() { }
+
+        public Reservation(int numberRoom, DateTime checkin, DateTime checkout)
+        {
+            if (checkout <= checkin)
+            {
+                throw new Exception("Check-out date must be after check-in date");
+            } else
+            {
+                this.checkin = checkin;
+                this.checkout = checkout;
+            }
+            if (numberRoom > 0)
+            {
+                this.numberRoom = numberRoom;
+            } else
+            {
+                throw new Exception("The room number needs > 0");
+            }
+        }
+
+        public int duration()
+        {
+            TimeSpan timestamp = this.checkout.Subtract(this.checkin);
+            return (int)timestamp.TotalDays;
+        }
+
+        public void updateDate(DateTime checkin, DateTime checkout) {
+            DateTime now = DateTime.Now;
+            if (checkout < now || checkin < now)
+            {
+                throw new Exception("Reservation dates for update must be future dates");
+            } else if (checkout <= checkin)
+            {
+                throw new Exception("Check-out date must be after check-in date");
+            }
+            this.checkin = checkin;
+            this.checkout = checkout;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + this.numberRoom
+                + ", check-in: "
+                + checkin.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + checkout.ToString("dd/MM/yyyy")
+                + ", "
+                + duration()
+                + " nights";
+        }
+    }
+}
---
+++ rooms/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using rooms.Entities;
+
+namespace rooms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Número do quarto? ");
+            int numberRoom = int.Parse(Console.ReadLine());
+            Console.Write("Data de checkin? (dd/mm/yyyy): ");
+            DateTime checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Data de checkout? (dd/mm/yyyy): ");
+            DateTime checkout = DateTime.Parse(Console.ReadLine());
+            Reservation reservation = new Reservation(numberRoom, checkin, checkout);
+            Console.WriteLine(reservation);
+
+            Console.WriteLine();
+
+            Console.Write("Insira a nova data de checkin? (dd/mm/yyyy): ");
+            checkin = DateTime.Parse(Console.ReadLine());
+            Console.Write("Insira a nova data de checkout? (dd/mm/yyyy): ");
+            checkout = DateTime.Parse(Console.ReadLine());
+            reservation.updateDate(checkin, checkout);
+            Console.WriteLine(reservation);
+        }
+    }
+}
